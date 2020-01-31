ebf1b1b9656b6b2345df4fcdb1ca6b30b729242e
Kazi Jawad <kazi_Jawad@outlook.com>
2020-01-31 17:51:10 -0500

    Initial Commit

---
+++ Source/GameJam/GameJam.cpp
@@ -0,0 +1,9 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "GameJam.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GameJam, "GameJam" );
+
+DEFINE_LOG_CATEGORY(LogGameJam)
+ 
